472806c9be61a3dfc3449eb7a46e9d74c35c5c89
DragonAI <plugmeinplease@gmail.com>
2019-08-20 13:37:12 -0700

    first commit

---
+++ Market/Script/Script-1x3 PPL_Gabby_1.js
@@ -0,0 +1,3 @@
+<div class="answer_list" > WELCOME </div>
+<input type="button" name="answer" value="Show Text Field" onclick="onButtonClick()" />
+<input class="hide" type="text" id="textInput" value="..." />
